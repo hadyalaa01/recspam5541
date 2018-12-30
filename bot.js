b1c2b89cd16dd5e4ac084a220190778c02fac1ce
hadyalaa01 <46203898+hadyalaa01@users.noreply.github.com>
2018-12-30 21:06:12 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("528267684106862630")
+setInterval(function() {
+channel.send(`iTzSemba.isHere`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
